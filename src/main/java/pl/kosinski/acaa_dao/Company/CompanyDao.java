fcd322dfc3c134c701712ecb776c257f089f7fd6
Ivoriel <tkivor@gmail.com>
2022-08-31 20:12:28 +0200

    CompanyDao moved to the ACAA-dao library.

---
+++ src/main/java/pl/kosinski/acaa_dao/Company/CompanyDao.java
@@ -0,0 +1,12 @@
+package pl.kosinski.acaa_dao.Company;
+
+
+import pl.kosinski.acaa_model.Client.Client;
+
+public class CompanyDao {
+
+    private Long id;
+    private String name;
+    private Client client;
+
+}

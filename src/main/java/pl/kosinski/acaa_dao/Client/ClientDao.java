9a0b5edd00ec16332eee3abe6ef7d8e8ce5e5abe
Ivoriel <tkivor@gmail.com>
2022-09-21 09:10:51 +0200

    Address added to ClienDao.

--- src/main/java/pl/kosinski/acaa_dao/Client/ClientDao.java
+++ src/main/java/pl/kosinski/acaa_dao/Client/ClientDao.java
@@ -1,8 +1,11 @@
 package pl.kosinski.acaa_dao.Client;
 
+import pl.kosinski.acaa_model.Address.Address;
+
 public class ClientDao {
 
     private long id;
     private String name;
+    private Address address;
 
 }

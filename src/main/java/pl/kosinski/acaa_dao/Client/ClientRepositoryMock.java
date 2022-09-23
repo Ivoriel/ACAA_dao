a0588cc07cc2afc82d87b613eb1d2791f2fec727
Ivoriel <tkivor@gmail.com>
2022-09-23 10:56:27 +0200

    ClientRepositoryMock created.

---
+++ src/main/java/pl/kosinski/acaa_dao/Client/ClientRepositoryMock.java
@@ -0,0 +1,11 @@
+package pl.kosinski.acaa_dao.Client;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * The purpose of this class is to operate as a mock db during development.
+ */
+
+@Component
+public class ClientRepositoryMock implements ClientRepository {
+}

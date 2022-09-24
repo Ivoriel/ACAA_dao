61454142d893f31e540f86213d8cc3050cc1a0a2
Ivoriel <tkivor@gmail.com>
2022-09-24 18:39:20 +0200

    CompanyRepositoryMock created.

---
+++ src/main/java/pl/kosinski/acaa_dao/Company/CompanyRepositoryMock.java
@@ -0,0 +1,11 @@
+package pl.kosinski.acaa_dao.Company;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * The purpose of this class is to operate as a mock db during development.
+ */
+
+@Component
+public class CompanyRepositoryMock {
+}

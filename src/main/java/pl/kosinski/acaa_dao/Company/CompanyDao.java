7012e7892bb85870abbc7ba7533c87f96287ecbd
Ivoriel <tkivor@gmail.com>
2022-10-02 17:05:16 +0200

    Added constructor to CompanyDao

--- src/main/java/pl/kosinski/acaa_dao/Company/CompanyDao.java
+++ src/main/java/pl/kosinski/acaa_dao/Company/CompanyDao.java
@@ -1,14 +1,20 @@
 package pl.kosinski.acaa_dao.Company;
 
-
 import pl.kosinski.acaa_model.Address.Address;
 import pl.kosinski.acaa_model.Client.Client;
 
 public class CompanyDao {
 
-    private Long id;
+    private long id;
     private String name;
     private Client client;
     private Address address;
+
+    CompanyDao(long id, String name, Client client, Address address) {
+        this.id = id;
+        this.name = name;
+        this.client = client;
+        this.address = address;
+    }
 
 }

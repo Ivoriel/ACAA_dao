5075774e62401a338e3eef98a1fb310a0bf8a712
Ivoriel <tkivor@gmail.com>
2022-10-02 16:52:58 +0200

    Added a hashMap to act as a mock db for dev purposes.

--- src/main/java/pl/kosinski/acaa_dao/Company/CompanyRepositoryMock.java
+++ src/main/java/pl/kosinski/acaa_dao/Company/CompanyRepositoryMock.java
@@ -2,6 +2,9 @@ package pl.kosinski.acaa_dao.Company;
 
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+
 /**
  * The purpose of this class is to operate as a mock db during development.
  */
@@ -9,9 +12,27 @@ import org.springframework.stereotype.Component;
 @Component
 public class CompanyRepositoryMock implements CompanyRepository{
 
+    HashMap CompanyDb;
+
+    @PostConstruct
+    public void init() {
+        populateCompanyDb();
+    }
+
     @Override
     public CompanyDao getCompany(long id) {
         return null;
+    }
+
+    private void populateCompanyDb() {
+        for (long i = 0; i < 9; i++) {
+            CompanyDb.put(i, generateCompany(i));
+        }
+    }
+
+    private CompanyDao generateCompany(long id) {
+        CompanyDao dao = new CompanyDao();
+        return dao;
     }
 
 }

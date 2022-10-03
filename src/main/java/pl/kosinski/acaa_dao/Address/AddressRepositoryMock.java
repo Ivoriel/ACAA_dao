4475b694b78927c6ffc057bb7a87b2a1d6d84e94
Ivoriel <tkivor@gmail.com>
2022-10-03 12:23:35 +0200

    Mock db, init() and basic address generation added to AddressReporitoryMock

--- src/main/java/pl/kosinski/acaa_dao/Address/AddressRepositoryMock.java
+++ src/main/java/pl/kosinski/acaa_dao/Address/AddressRepositoryMock.java
@@ -1,4 +1,34 @@
 package pl.kosinski.acaa_dao.Address;
 
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+
+/**
+ * The purpose of this class is to operate as a mock db during development.
+ */
+
+@Component
 public class AddressRepositoryMock implements AddressRepository{
+
+    HashMap AddressDb;
+
+    @PostConstruct
+    public void init() {
+        populateAddressDb();
+    }
+
+    private void populateAddressDb(){
+        for (long i = 1; i < 10; i++) {
+            generateAddress(i);
+        }
+    }
+
+    private AddressDao generateAddress(long id) {
+        AddressDao dao = new AddressDao(id, generateCountry(), generateMunicipality(), generateRegion(), generateZipCode(),
+                generateStreet(), generateBuildingNumber(), generateAdditionalIdentifier());
+        return dao;
+    }
+
 }

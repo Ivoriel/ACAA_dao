ab497f0fe65bf1795233e1b1af3c961a7576b799
Ivoriel <tkivor@gmail.com>
2022-10-06 13:41:47 +0200

    Added saveAddress method to AddressRepository

--- src/main/java/pl/kosinski/acaa_dao/Address/AddressRepository.java
+++ src/main/java/pl/kosinski/acaa_dao/Address/AddressRepository.java
@@ -2,6 +2,8 @@ package pl.kosinski.acaa_dao.Address;
 
 public interface AddressRepository {
 
+    AddressDao saveAddress(AddressDao addressDao);
+
     AddressDao getAddress(long id);
 
     void deleteAddress(long id);

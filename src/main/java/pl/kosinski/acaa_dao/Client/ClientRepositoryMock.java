d7c3aa80adef0c99485e4b0fdc3ccd86c17b5f19
Ivoriel <tkivor@gmail.com>
2022-10-11 10:18:19 +0200

    Added methods populating the mock db

--- src/main/java/pl/kosinski/acaa_dao/Client/ClientRepositoryMock.java
+++ src/main/java/pl/kosinski/acaa_dao/Client/ClientRepositoryMock.java
@@ -2,6 +2,9 @@ package pl.kosinski.acaa_dao.Client;
 
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+
 /**
  * The purpose of this class is to operate as a mock db during development.
  */
@@ -9,9 +12,27 @@ import org.springframework.stereotype.Component;
 @Component
 public class ClientRepositoryMock implements ClientRepository {
 
+    HashMap ClientDb;
+
+    @PostConstruct
+    public void init() {
+        populateClientDb();
+    }
+
     @Override
     public ClientDao getClient(long id) {
         return null;
+    }
+
+    private void populateClientDb() {
+        for (long i = 1; i < 10; i++) {
+            generateClient(i);
+        }
+    }
+
+    private ClientDao generateClient(long id) {
+        ClientDao dao = new ClientDao(id, generateName(), genrateAddress());
+        return dao;
     }
 
 }

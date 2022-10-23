dd601e39f2d0ac892441215077b966385a3dd61e
Ivoriel <tkivor@gmail.com>
2022-10-24 00:06:15 +0200

    Changed getAddress return optional.

--- src/main/java/pl/kosinski/acaa_dao/Address/AddressRepository.java
+++ src/main/java/pl/kosinski/acaa_dao/Address/AddressRepository.java
@@ -1,10 +1,12 @@
 package pl.kosinski.acaa_dao.Address;
 
+import java.util.Optional;
+
 public interface AddressRepository {
 
     AddressDao saveAddress(AddressDao addressDao);
 
-    AddressDao getAddress(long id);
+    Optional<AddressDao> getAddress(long id);
 
     void deleteAddress(long id);
 

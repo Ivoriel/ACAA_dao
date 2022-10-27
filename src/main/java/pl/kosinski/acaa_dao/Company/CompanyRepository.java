3a3e2bd0f9c25381e2bd789d9beeadfae89ce4ea
Ivoriel <tkivor@gmail.com>
2022-10-27 02:37:20 +0200

    SaveCompany method added.

--- src/main/java/pl/kosinski/acaa_dao/Company/CompanyRepository.java
+++ src/main/java/pl/kosinski/acaa_dao/Company/CompanyRepository.java
@@ -2,6 +2,8 @@ package pl.kosinski.acaa_dao.Company;
 
 public interface CompanyRepository {
 
+    CompanyDao saveCompany(CompanyDao companyDao);
+
     CompanyDao getCompany(long id);
 
     void deleteCompany(long id);

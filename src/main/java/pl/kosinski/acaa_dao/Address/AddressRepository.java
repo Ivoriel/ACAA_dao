88938438bcbbeb334105952602d08f61b26a95ad
Ivoriel <tkivor@gmail.com>
2022-11-06 12:30:42 +0100

    Added generate() method returning addressDto

--- src/main/java/pl/kosinski/acaa_dao/Address/AddressRepository.java
+++ src/main/java/pl/kosinski/acaa_dao/Address/AddressRepository.java
@@ -1,5 +1,7 @@
 package pl.kosinski.acaa_dao.Address;
 
+import pl.kosinski.acaa_dto.AddressDto;
+
 import java.util.Optional;
 
 public interface AddressRepository {
@@ -11,5 +13,7 @@ public interface AddressRepository {
     void deleteAddress(long id);
 
     int size();
+
+    AddressDto generate();
 
 }

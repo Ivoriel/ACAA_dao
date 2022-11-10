15000aea7a33a9709c8d91d131e0af1b4916bdfc
Ivoriel <tkivor@gmail.com>
2022-11-10 01:39:22 +0100

    Refactored method names

--- src/main/java/pl/kosinski/acaa_dao/Company/CompanyRepository.java
+++ src/main/java/pl/kosinski/acaa_dao/Company/CompanyRepository.java
@@ -2,10 +2,10 @@ package pl.kosinski.acaa_dao.Company;
 
 public interface CompanyRepository {
 
-    CompanyDao saveCompany(CompanyDao companyDao);
+    CompanyDao save(CompanyDao companyDao);
 
-    CompanyDao getCompany(long id);
+    CompanyDao get(long id);
 
-    void deleteCompany(long id);
+    void delete(long id);
 
 }
